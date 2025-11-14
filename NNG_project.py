2b39c502e8e0a89e98eac34fbc77700c393dbc87
jordan78906 <jordan78906@yahoo.com>
2025-11-14 01:10:59 -0600

    Update

--- NNG_project.py
+++ NNG_project.py
@@ -1,5 +1,12 @@
 import numpy as np
 import pandas as pd
+from scipy import stats
+
+import seaborn as sns
+import plotly.express as px
+import matplotlib.pyplot as plt
+
+
 #import math 
 #import streamlit as st      Implement at end
 
@@ -11,12 +18,11 @@ def nelson_siege_formula (B_0, B_1, B_2, L, T):
     # L     lambda= 
     # T     time  = Maturity
     yield_pct = B_0 + B_1((1 - np.exp(-L * T))/ L * T) + B_2((1 - np.exp(-L * T))/(L * T) - np.exp(-L * T))
-    T_nsf = T
-    return yield_pct, T_nsf
+    return yield_pct, T
 
 
-def money(maturity, cupon, yield_pct, price, T_nsf): 
-    value = 100/((1 + yield_pct ) ** maturity) + (cupon/T_nsf)
+def buy_price(maturity, cupon, yield_pct, price, T): 
+    value = 100/((1 + yield_pct ) ** maturity) + (cupon/T)
     return value
 
 #Hard coded values for now
@@ -24,13 +30,20 @@ B_0 = 0.0000
 B_1 = 0.0000
 B_2 = 0.0000
 L   = 0.0000
-T   = 0
+T   = 30
+T_bond   = 10
+###########################
 
-dict_yield = {}
-for i in range (1, 31):
-    dict_yield [i] = 0.0000
+test_raw = pd.read_csv('./nelson_siegel_curve_project/Test_data.csv')
 
+print (f'\nShape:{test_raw.shape}\n')
+print (test_raw.head(10))
 
+#dict_yield = {}
+#for i in range (1, 31):
+#    dict_yield [i] = 0.0000
+
+#print (data_df)
 
 #for v in range (1, 31):
 #    print (f"{v}      {dict_yield[v]}")
